50c60a57af357f1fea67041e45dcb5b1ac822489
MVOrange55 <vishalkashyap4777@gmail.com>
2026-06-05 17:31:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def run_hr_engine(df, holidays, corrections):
             df_w.at[idx+1, str(c['date'])] = c['in']
             df_w.at[idx+2, str(c['date'])] = c['out']
     dates = [c for c in df_w.columns if str(c).replace('.0','').strip().isdigit()]
-    sundays = [3, 10, 17, 24] 
+    sundays = [7, 14, 21, 28] 
     res_m, res_s, res_o, res_ex, res_mi = [], [], [], [], []
     for eid in df_w[id_c].unique():
         if pd.isna(eid): continue
